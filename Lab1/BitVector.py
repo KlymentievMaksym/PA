6319a88ad6a95b0768e8198587dd40341fb87545
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-09-14 21:28:50 +0300

    create, insert, delete, search, clear, union, intersection are online

--- Lab1/BitVector.py
+++ Lab1/BitVector.py
@@ -1,35 +1,82 @@
+import numpy as np
+
+
 class BitVectorSet:
-    def __init__(self, size=1000, register_value=64):
+    def __init__(self, *start_numbers, size: int = 1000, register_value: int = 64):
         self.size = size
         self.register_value = register_value
-        self.bits = [[0]*register_value] * size
+        self.bits = [0b0] * size
+        self.max_value = register_value * size - 1
+        self.univesum = [bin(register_value - 1)] * size
+        for value in start_numbers:
+            self.insert(value)
 
     def time_check(self):
         raise NotImplementedError
 
-    def insert(self, value):
-        raise NotImplementedError
+    def insert(self, value: int):
+        bit_vector_to_place = int(np.floor(value / self.register_value))
+        bit_to_place = value % self.register_value
 
-    def delete(self, value):
-        raise NotImplementedError
+        self.bits[bit_vector_to_place] = self.bits[bit_vector_to_place] | (1 << bit_to_place)
 
-    def search(self, value):
-        raise NotImplementedError
+    def delete(self, value: int):
+        bit_vector_to_delete = int(np.floor(value / self.register_value))
+        bit_to_delete = value % self.register_value
+
+        self.bits[bit_vector_to_delete] = self.bits[bit_vector_to_delete] & ~(1 << bit_to_delete)
+
+    def search(self, value: int):
+        bit_vector_to_check = int(np.floor(value / self.register_value))
+        bit_to_check = value % self.register_value
+
+        return self.bits[bit_vector_to_check] & (1 << bit_to_check)
 
     def clear(self):
-        raise NotImplementedError
+        old_bit_vector_set = self.bits.copy()
+
+        self.bits = [0b0] * self.size
+
+        return old_bit_vector_set
 
     def union(self, other: 'BitVectorSet'):
-        raise NotImplementedError
+        old_bit_vector_set = self.bits.copy()
+
+        for bit_vector_number in range(self.size):
+            self.bits[bit_vector_number] = self.bits[bit_vector_number] | other.bits[bit_vector_number]
+
+        return old_bit_vector_set
 
     def intersection(self, other: 'BitVectorSet'):
-        raise NotImplementedError
+        old_bit_vector_set = self.bits.copy()
+
+        for bit_vector_number in range(self.size):
+            self.bits[bit_vector_number] = self.bits[bit_vector_number] & other.bits[bit_vector_number]
+
+        return old_bit_vector_set
 
     def set_difference(self, other: 'BitVectorSet'):
-        raise NotImplementedError
+        old_bit_vector_set = self.bits.copy()
+
+        for bit_vector_number in range(self.size):
+            self.bits[bit_vector_number] = self.bits[bit_vector_number] & other.bits[bit_vector_number]
+
+        return old_bit_vector_set
 
     def sym_difference(self, other: 'BitVectorSet'):
         raise NotImplementedError
 
     def is_subset(self, other: 'BitVectorSet'):
         raise NotImplementedError
+
+    def __str__(self):
+        str_to_give = "[ "
+        for bit in self.bits:
+            str_to_give += str(bin(bit)) + " "
+        str_to_give += "]"
+        return str_to_give
+
+
+if __name__ == '__main__':
+    bit_vector = BitVectorSet(1, 3, size=2)
+    print(bit_vector)
--- Lab1/BitVector_test.py
+++ Lab1/BitVector_test.py
@@ -4,34 +4,88 @@ from BitVector import BitVectorSet
 
 class TestBitVectorSet(unittest.TestCase):
     def test_create(self):
-        pass
+        test_bit_vector = BitVectorSet(1, 3)
+
+        self.assertEqual(test_bit_vector.bits, [10] + [0] * 999)
 
     def test_insert(self):
-        pass
+        test_bit_vector = BitVectorSet(1, 3)
+
+        test_bit_vector.insert(5)
+
+        self.assertEqual(test_bit_vector.bits, [42] + [0] * 999)
 
     def test_delete(self):
-        pass
+        test_bit_vector = BitVectorSet(1, 3)
 
-    def test_search(self):
-        pass
+        test_bit_vector.delete(3)
 
-    def test_clear(self):
-        pass
+        self.assertEqual(test_bit_vector.bits, [2] + [0] * 999)
+
+    def test_search_already_created_numbers(self):
+        test_bit_vector = BitVectorSet(1, 3)
+
+        self.assertEqual(test_bit_vector.search(3), 8)
+
+    def test_search_not_created_numbers(self):
+        test_bit_vector = BitVectorSet(1, 3)
+
+        test_bit_vector.insert(5)
+
+        self.assertEqual(test_bit_vector.search(5), 32)
+
+    def test_clear_already_created_numbers(self):
+        test_bit_vector = BitVectorSet(1, 3)
+
+        test_bit_vector.clear()
+
+        self.assertEqual(test_bit_vector.bits, [0] * 1000)
+
+    def test_clear_not_created_numbers(self):
+        test_bit_vector = BitVectorSet(1, 3)
+
+        test_bit_vector.insert(5)
+        test_bit_vector.clear()
+
+        self.assertEqual(test_bit_vector.bits, [0] * 1000)
 
     def test_union(self):
-        pass
+        test_bit_vector1 = BitVectorSet(1, 3)
+        test_bit_vector2 = BitVectorSet(2, 4)
+
+        test_bit_vector1.union(test_bit_vector2)
+
+        self.assertEqual(test_bit_vector1.bits, [30] + [0] * 999)
 
     def test_intersection(self):
-        pass
+        test_bit_vector1 = BitVectorSet(1, 3)
+        test_bit_vector2 = BitVectorSet(1, 4)
+
+        test_bit_vector1.intersection(test_bit_vector2)
+
+        self.assertEqual(test_bit_vector1.bits, [2] + [0] * 999)
 
     def test_set_difference(self):
-        pass
+        test_bit_vector1 = BitVectorSet(1, 3)
+        test_bit_vector2 = BitVectorSet(1, 4)
+
+        test_bit_vector1.set_difference(test_bit_vector2)
+
+        self.assertEqual(test_bit_vector1.bits, [8] + [0] * 999)
 
     def test_sym_difference(self):
-        pass
+        test_bit_vector1 = BitVectorSet(1, 3)
+        test_bit_vector2 = BitVectorSet(1, 4)
+
+        test_bit_vector1.sym_difference(test_bit_vector2)
+
+        self.assertEqual(test_bit_vector1.bits, [12] + [0] * 999)
 
     def test_is_subset(self):
-        pass
+        test_bit_vector1 = BitVectorSet(1, 2, 3)
+        test_bit_vector2 = BitVectorSet(1, 3)
+
+        self.assertEqual(test_bit_vector2.is_subset(test_bit_vector1), 1)
 
     def test_time_check(self):
         pass
