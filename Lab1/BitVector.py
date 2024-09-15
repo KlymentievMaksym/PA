8295d7d2778783d50712013508ba343c46f0ed66
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-09-15 17:00:45 +0300

    Time show finished

--- Lab1/BitVector.py
+++ Lab1/BitVector.py
@@ -11,16 +11,13 @@ class BitVectorSet:
         for value in start_numbers:
             self.insert(value)
 
-    def time_check(self):
-        raise NotImplementedError
-
     def check_values(self, value: int):
-        if not isinstance(value, int):
-            raise TypeError("Value is not an integer")
+        if not isinstance(value, (int, np.int32)):
+            raise TypeError(f"Value {value} is not an integer")
         elif value > self.max_value:
-            raise ValueError("Value is too big")
+            raise ValueError(f"Value {value} is too big")
         elif value < 0:
-            raise ValueError("Value is too small")
+            raise ValueError(f"Value {value} is too small")
 
     def check_sets(self, other: 'BitVectorSet'):
         if not isinstance(other, BitVectorSet):
@@ -33,7 +30,6 @@ class BitVectorSet:
             }
 
             for key, check in dict_to_check.items():
-                print(key, check)
                 can_be_compared -= not check
                 if not can_be_compared:
                     raise ValueError(f"{key} are not equal")
@@ -123,6 +119,55 @@ class BitVectorSet:
         return str_to_give
 
 
-if __name__ == '__main__':
-    bit_vector = BitVectorSet(1, 3, size=2)
-    print(bit_vector)
+if __name__ == "__main__":
+    np.random.seed(0)
+
+    list_of_18_lists = []
+    random_numbers = []
+
+    iterations_for_data = 1000
+    iterations_for_time = 300
+
+    size_start = 1000
+    size_end = 30000
+    size_step = 300
+
+    for j in range(size_end//size_step):
+        list_of_18_lists.append([np.random.randint(0, 64 * (size_step*j + size_start) - 1, iterations_for_data) for _ in range(iterations_for_data)])
+        random_numbers.append(np.random.randint(0, 64 * (size_step*j + size_start) - 1, iterations_for_data))
+
+    import time
+    import matplotlib.pyplot as plt
+
+    i_for_search = np.array([])
+    time_for_search = np.array([])
+    for i in range(size_start, size_end, size_step):
+        start = time.time()
+        for _ in range(iterations_for_time):
+            BitVectorSet(*(list_of_18_lists[0][i//500-1]), size=i).search(random_numbers[0][i//500-1])
+        end = time.time()
+        i_for_search = np.append(i_for_search, i)
+        time_for_search = np.append(time_for_search, ((end - start) / iterations_for_time * 10**3))
+    figure, axis = plt.subplots()
+    axis.plot(i_for_search, time_for_search)
+    axis.set_title("Search time")
+    axis.set_xlabel("Розмір множини")
+    axis.set_ylabel("Час виконання, мс")
+    plt.show()
+
+    i_for_search = np.array([])
+    time_for_search = np.array([])
+    for i in range(size_start, size_end, size_step):
+        index_for_second_bit_vector = int(np.random.randint(0, size_end//size_step, 1)[0])
+        start = time.time()
+        for _ in range(iterations_for_time):
+            BitVectorSet(*(list_of_18_lists[0][i//500-1]), size=i).sym_difference(BitVectorSet(*(list_of_18_lists[0][index_for_second_bit_vector]), size=i))
+        end = time.time()
+        i_for_search = np.append(i_for_search, i)
+        time_for_search = np.append(time_for_search, ((end - start) / iterations_for_time * 10**3))
+    figure, axis = plt.subplots()
+    axis.plot(i_for_search, time_for_search)
+    axis.set_title("SymDifference time")
+    axis.set_xlabel("Розмір множини")
+    axis.set_ylabel("Час виконання, мс")
+    plt.show()
