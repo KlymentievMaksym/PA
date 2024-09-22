5a32b65f23b352127c56003e0a650710d663e765
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-09-22 16:49:11 +0300

    Initial commit 2

--- Lab2/Graph.py
+++ Lab2/Graph.py
@@ -1,18 +1,113 @@
 class Graph:
     def __init__(self, number_of_vertices):
+        # Matrix realization
         self.number_of_vertices = number_of_vertices
         self.graph = [[0]*number_of_vertices]*number_of_vertices
+
+    def convertator(self, type_of_realization: str):
+        pass
+
+    def add_vertex(self, v):
+        pass
+
+    def add_edge(self, v1, v2):
+        pass
+
+    def remove_vertex(self, v):
+        pass
+
+    def remove_edge(self, v1, v2):
+        pass
+
+
+class UndirectedGraph(Graph):
+    def __init__(self, number_of_vertices):
+        super().__init__(number_of_vertices)
+
+    def convertator(self, type_of_realization: str):
+        raise NotImplementedError
 
     def add_vertex(self, v):
         raise NotImplementedError
 
     def add_edge(self, v1, v2):
-        self.graph[v1][v2] = 1
-        self.graph[v2][v1] = 1
+        raise NotImplementedError
 
-    def delete_vertex(self, v):
+    def remove_vertex(self, v):
         raise NotImplementedError
 
     def remove_edge(self, v1, v2):
-        self.graph[v1][v2] = 0
-        self.graph[v2][v1] = 0
+        raise NotImplementedError
+
+
+
+class DirectedGraph(Graph):
+    def __init__(self, number_of_vertices):
+        super().__init__(number_of_vertices)
+
+    def convertator(self, type_of_realization: str):
+        raise NotImplementedError
+
+    def add_vertex(self, v):
+        raise NotImplementedError
+
+    def add_edge(self, v1, v2):
+        raise NotImplementedError
+
+    def remove_vertex(self, v):
+        raise NotImplementedError
+
+    def remove_edge(self, v1, v2):
+        raise NotImplementedError
+
+
+class WeightedGraph(Graph):
+    def __new__(cls, type_of_graph):
+        if cls is not WeightedGraph:
+            return super().__new__(cls)
+        class_map = {
+            'Undirected': WeightedGraphUndirected, 
+            'Directed': WeightedGraphDirected
+            }
+        cls = class_map[type_of_graph]
+        return super().__new__(cls, type_of_graph)
+
+class WeightedGraphUndirected(WeightedGraph, UndirectedGraph):
+    def __init__(self, number_of_vertices):
+        super().__init__(number_of_vertices)
+
+    def add_edge(self, v1, v2, weight):
+        super().add_edge(v1, v2, weight)
+
+    def remove_edge(self, v1, v2):
+        super().remove_edge(v1, v2)
+
+    def add_vertex(self, v):
+        super().add_vertex(v)
+
+    def remove_vertex(self, v):
+        super().remove_vertex(v)
+
+
+class WeightedGraphDirected(WeightedGraph, DirectedGraph):
+    def __init__(self, number_of_vertices):
+        super().__init__(number_of_vertices)
+
+    def add_edge(self, v1, v2, weight):
+        super().add_edge(v1, v2, weight)
+
+    def remove_edge(self, v1, v2):
+        super().remove_edge(v1, v2)
+
+    def add_vertex(self, v):
+        super().add_vertex(v)
+
+    def remove_vertex(self, v):
+        super().remove_vertex(v)
+
+
+if __name__ == '__main__':
+    undirected_graph = UndirectedGraph(5)
+    directed_graph = DirectedGraph(5)
+    weighted_graph_dn = WeightedGraph('Undirected')(5)
+    weighted_graph_d = WeightedGraph('Directed')(5)
--- Lab2/Graph_test.py
+++ Lab2/Graph_test.py
@@ -1,36 +1,29 @@
 import unittest
-from Graph import Graph
+from Graph import *
 
 
 class TestGraph(unittest.TestCase):
-    def test_init(self):
-        test_graph = Graph(5)
-        self.assertEqual(test_graph.number_of_vertices, 5)
+    def test_init_undirected_graph(self):
+        graph = UndirectedGraph(5)
+        items = (
+            {'case': 'Adding a vertex', 'method': graph.add_vertex,'v': [5], 'expected': 1},
+            {'case': 'Adding an edge', 'method': graph.add_edge,'v': [1, 2], 'expected': 1},
+            {'case': 'Removing a vertex', 'method': graph.remove_vertex,'v': [1], 'expected': 0},
+            {'case': 'Removing an edge', 'method': graph.remove_edge,'v': [1, 2], 'expected': 0},
+        )
+        for item in items:
+            with self.subTest(item['case']):
+                result = item['method'](*item['v'])
+                self.assertEqual(result, item['expected'])
 
-    def test_add_vertex(self):
-        test_graph = Graph(5)
-        test_graph.add_vertex(5)
-        self.assertEqual(test_graph.graph[5][5], 0)
+    def test_init_directed_graph(self):
+        raise NotImplementedError
 
-    def test_add_edge(self):
-        test_graph = Graph(5)
-        test_graph.add_edge(0, 1)
-        self.assertEqual(test_graph.graph[0][1], 1)
-        self.assertEqual(test_graph.graph[1][0], 1)
+    def test_init_weighted_graph_undirected(self):
+        raise NotImplementedError
 
-    def test_delete_vertex(self):
-        test_graph = Graph(5)
-        test_graph.add_vertex(5)
-        test_graph.delete_vertex(5)
-        self.assertEqual(test_graph.graph[5][5], 0)
-
-    def test_remove_edge(self):
-        test_graph = Graph(5)
-        test_graph.add_edge(0, 1)
-        test_graph.remove_edge(0, 1)
-        self.assertEqual(test_graph.graph[0][1], 0)
-        self.assertEqual(test_graph.graph[1][0], 0)
-
+    def test_init_weighted_graph_directed(self):
+        raise NotImplementedError
 
 if __name__ == '__main__':
     unittest.main()
