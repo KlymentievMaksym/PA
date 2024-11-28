fc714bd53bd1cc3ae82aa0f5c762dd92ac1c5d64
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-11-28 18:22:53 +0200

    Started a) and b)

--- Lab7/QuickSort.py
+++ Lab7/QuickSort.py
@@ -1,10 +1,75 @@
+from numpy import random
+import time
+import sys
+
+
 class QuickSort:
-    _PIVOT = []
+    def __init__(self):
+        self._PIVOT = {'LAST': self._last_pivot, 'RANDOM': self._random_pivot, 'FIRST': self._first_pivot, 'MID': self._mid_pivot, 'MID3': self._mid3_pivot}
+        self._SCHEME = {'LOMUTE': self._lomute, 'HOAR': self._hoar}
+
+    def _swap(self, array, i, j):
+        array[i], array[j] = array[j], array[i]
+        return array
+
+    def _last_pivot(self, array):
+        return array[-1]
+
+    def _first_pivot(self, array):
+        return array[0]
+
+    def _mid_pivot(self, array):
+        return array[len(array) // 2]
+
+    def _mid3_pivot(self, array):
+        array_copy = array.copy()
+        mid3 = []
+        for i in range(3):
+            mid3.append(array_copy.pop(random.randint(0, len(array))))
+        for i in range(3):
+            for j in range(i+1, 3):
+                if mid3[i] > mid3[j]:
+                    mid3[i], mid3[j] = mid3[j], mid3[i]
+        return mid3[1]
+
+    def _choose_pivot(self, array, pivot):
+        """4 методи вибору опорного елемента"""
+        if pivot.upper() in self._PIVOT.keys():
+            return self._PIVOT[pivot.upper()](array)
+        else:
+            raise ValueError("Pivot can be only LAST, RANDOM, FIRST, MID or MID3")
+
+    def _choose_scheme(self, scheme):
+        """2 схеми розбиття"""
+        if scheme.upper() in self._SCHEME.keys():
+            return self._SCHEME[scheme.upper()]
+        else:
+            raise ValueError("Scheme can be only LOMUTE or HOAR")
+
+    def _lomute(self, array, pivot):
+        """Алгоритм швидкого сортування зi схемою розбиття Ломуто"""
+        pass
+
+    def _hoar(self, array, pivot):
+        """Алгоритм швидкого сортування зi схемою розбиття Гоара"""
+        pass
+
     def standard_quicksort(self, array, scheme='lomute', pivot='last', time_count=False, details_need=False, inplace=False, _equations=0, _memory=0, _swaps=0):
-        """а) Реалiзувати алгоритм швидкого сортування зi схемами розбиття Ломуто та Гоара, а
-також вибором опорного елемента як останнього елемента, випадкового елемента,
-медiани першого, останнього та середнього елементiв i медiани трьох випадкових
-елементiв (з пiдтримкою обчислення часу виконання, кiлькостi проведених порiвнянь,
-операцiй переставляння елементiв та використаної пам’ятi). (Загальна кiлькiсть
-модифiкацiй дорiвнює 8 — 2 схеми розбиття times 4 методи вибору опорного елемента.)"""
-    pass
+        """а) Реалiзувати Алгоритм швидкого сортування (з пiдтримкою обчислення часу виконання, кiлькостi проведених порiвнянь, операцiй переставляння елементiв та використаної пам’ятi)."""
+        pivot = self._choose_pivot(array, pivot)
+        scheme = self._choose_scheme(scheme)
+        if time_count:
+            start = time.time()
+            _memory += sys.getsizeof(start)
+        ### MAIN PART ###
+        if time_count:
+            time_result = time.time() - start
+            _memory += sys.getsizeof(time_result)
+            to_return = {"Time": time_result, "Equations": _equations, "Memory": _memory, "Swaps": _swaps, "Name": "StandardQuicksort"}
+        else:
+            to_return = {"Equations": _equations, "Memory": _memory, "Swaps": _swaps, "Name": "StandardQuicksort"}
+        return to_return
+        
+
+if __name__ == "__main__":
+    print("Hello")
---
+++ Lab7/RandomLists.py
@@ -0,0 +1,68 @@
+# б ) Пiдготувати данi рiзного розмiру для проведення порiвняльного аналiзу. Мають бути
+# набори однакових елементiв, повнiстю вiдсортованих даних, випадкових даних, майже
+# вiдсортованих, вiдсортованих в зворотному порядку, “трикутнi” (перша половина є строго
+# висхiдною послiдовнiстю, а друга половина є дзеркальним вiдображенням першої) та лише
+# з декiлькома рiзними значеннями всiх розмiрiв. Розмiр даних має бути з достатнього
+# дiапазону, щоб побачити тенденцiю, й повинен мiстити такi, на яких можна побачити
+# рiзницю. Можна взяти обмеження за часом виконання вiд 0.01с до 30с.
+
+
+from numpy import random
+
+
+class RandomLists:
+    def __init__(self, n: int, type: str, *args, **kwargs):
+        self._TYPES = {'sorted': self.sorted, 'random': self.random, 'almostsorted': self.almostsorted, 'reverse': self.reverse, 'somenumbers': self.somenumbers, 'triangular': self.triangular}
+        if type not in self._TYPES:
+            raise ValueError(f'Invalid type: {type}, excpected one of {list(self._TYPES.keys())}')
+
+        self.n = n
+        if self.n <= 0:
+            raise ValueError(f'Invalid n: {self.n}, expected positive integer')
+
+        self.list = self._TYPES[type](*args, **kwargs)
+    
+    def sorted(self, *args, **kwargs):
+        start = kwargs.get('start', 0)
+        random_start = random.randint(start, self.n)
+        random_step = random.randint(start, self.n)
+        n = 0
+        to_return = []
+        while n != self.n:
+            to_return.append(random_start)
+            random_start += random_step
+            n += 1
+        return to_return
+
+    def random(self, *args, **kwargs):
+        return list(random.randint(0, self.n, self.n))
+
+    def almostsorted(self, disorder_level: float = 0.05, *args, **kwargs):
+        to_return = self.sorted(*args, **kwargs)
+
+        num_swaps = int(disorder_level * self.n)
+
+        for _ in range(num_swaps):
+            i, j = random.randint(0, self.n-1), random.randint(0, self.n-1)
+            to_return[i], to_return[j] = to_return[j], to_return[i]
+        return to_return
+
+    def reverse(self, *args, **kwargs):
+        return self.sorted(*args, **kwargs)[::-1]
+
+    def somenumbers(self, range_start=1, range_end=5, *args, **kwargs):
+        random_start = random.randint(0, self.n)
+        random_step = random.randint(range_start, range_end)
+        return list(random.randint(random_start, random_start + random_step, self.n))
+
+    def triangular(self, *args, **kwargs):
+        sorted = self.sorted(*args, **kwargs)
+        reversed = sorted[::-1]
+        return sorted[:self.n//2] + reversed[self.n//2:]
+
+    def __str__(self):
+        return str(self.list)
+
+if __name__ == '__main__':
+    rng = RandomLists(10, 'triangular', start=1)
+    print(rng)
