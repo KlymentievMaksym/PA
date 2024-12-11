1f4f0c18d47c7dfeeaa09e90ef1aae506197c70b
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-12-11 21:15:18 +0200

    Finished code, latex left

--- Lab7/QuickSort.py
+++ Lab7/QuickSort.py
@@ -6,7 +6,7 @@ import time
 import sys
 import os
 
-sys.setrecursionlimit(1500)
+sys.setrecursionlimit(30000)
 
 if not os.path.exists('./lab7/Results'):
     os.mkdir('./lab7/Results')
@@ -16,7 +16,7 @@ class QuickSort:
     def __init__(self):
         """Словники для вибору опорного елемента та схеми розбиття"""
         self._PIVOT = {'LAST': self._last_pivot, 'RANDOM': self._random_pivot, 'MID': self._mid_pivot, 'MID3': self._mid3_pivot}
-        self._SCHEME = {'LOMUTE': self._lomute, 'HOAR': self._hoar, 'DIJKSTRA': self._dijkstra}
+        self._SCHEME = {'LOMUTE': self._lomute, 'HOAR': self._hoar, 'DIJKSTRA': self._dijkstra, 'DUAL': self._dual_pivot_quicksort}
 
     def _swap(self, array, i, j):
         """Функцiя перестановки індексів двох елементiв"""
@@ -46,16 +46,13 @@ class QuickSort:
         """Медіана з трьох елементів (Відсортованих?)"""
         array_copy = array.copy()
         mid3 = []
-        mid3_indexes = []
-        for i in range(3):
-            mid3_indexes.append(random.randint(0, len(array_copy)))
-            mid3.append(array_copy.pop(mid3_indexes[i]))
-        for i in range(3):
-            for j in range(i+1, 3):
+        for i in range(min(3, len(array))):
+            mid3.append(array_copy.pop(random.randint(0, len(array_copy))))
+        for i in range(min(3, len(array))):
+            for j in range(i+1, min(3, len(array))):
                 if mid3[i] > mid3[j]:
                     self._swap(mid3, i, j)
-                    self._swap(mid3_indexes, i, j)
-        return mid3_indexes[1]
+        return array.index(mid3[1])
 
     def _choose_pivot(self, array, pivot):
         """4 методи вибору опорного елемента"""
@@ -69,7 +66,7 @@ class QuickSort:
         if scheme.upper() in self._SCHEME.keys():
             return self._SCHEME[scheme.upper()]
         else:
-            raise ValueError("Scheme can be only LOMUTE or HOAR")
+            raise ValueError(f"Scheme can be only from {list(self._SCHEME.keys())}")
 
     def _standard_check(self, array, _equations, _memory, _swaps):
         if len(array) == 2:
@@ -89,7 +86,9 @@ class QuickSort:
 
         pivot = self._choose_pivot(array, pivot_type)
 
-        array_pivot = array.pop(pivot)
+        # array_pivot = array.pop(pivot)
+        array_pivot = array[pivot]
+        array[pivot], array[-1] = array[-1], array[pivot]
         i, j = -1, 0
         _memory += sys.getsizeof(array_pivot)
         _memory += sys.getsizeof(i)
@@ -103,7 +102,7 @@ class QuickSort:
                 self._swap(array, i, j)
 
 
-        while j < len(array) - 1:
+        while j < len(array) - 2:
             j += 1
             _equations += 1
             if array[j] <= array_pivot:
@@ -111,7 +110,7 @@ class QuickSort:
                 if i != j:
                     _swaps += 1
                     self._swap(array, i, j)
-        array.append(array_pivot)
+        # array.append(array_pivot)
         i += 1
         _swaps += 1
         self._swap(array, i, -1)
@@ -150,17 +149,8 @@ class QuickSort:
                 _swaps += 1
                 self._swap(array, i, j)
         _equations += 1
-        # while array[i] <= t and i < j:
-        #     _equations += 1
-        #     i += 1
-        # _equations += 1
-        # while array[j] > t and i < j:
-        #     _equations += 1
-        #     j -= 1
-        # _equations += 1
-        # if i < j:
-        #     _swaps += 1
-        #     self._swap(array, i, j)
+
+        _equations += 1
         if j == len(array) - 1 and array[j] <= t:
             array.append(t)
             j += 1
@@ -188,7 +178,7 @@ class QuickSort:
         _memory += sys.getsizeof(j)
         _memory += sys.getsizeof(k)
 
-        while k < j:
+        while k <= j:
             _equations += 3
             if array[k] < t:
                 _equations -= 2
@@ -204,25 +194,78 @@ class QuickSort:
             elif array[k] == t:
                 k += 1
 
-        _equations += 3
-        if array[k] < t:
-            _equations -= 2
-            _swaps += 1
-            self._swap(array, i, k)
-            i += 1
-            k += 1
-        elif array[k] > t:
-            _equations -= 1
-            _swaps += 1
-            self._swap(array, k, j)
-            j -= 1
-        elif array[k] == t:
-            k += 1
 
         array[:i], _equations, _memory, _swaps = self._dijkstra(array[:i], pivot_type, _equations, _memory, _swaps, _depth + 1)
         array[k:], _equations, _memory, _swaps = self._dijkstra(array[k:], pivot_type, _equations, _memory, _swaps, _depth + 1)
 
-        # print(f"i: {i}, j: {j}, k: {k}, t: {t}")
+        return array, _equations, _memory, _swaps
+
+    def _dual_pivot_quicksort(self, array, pivot_type, _equations, _memory, _swaps, _depth=0):
+        """Алгоритм швидкого сортування зi схемою 3-стороннього розбиття Дуал-пiвота"""
+        _check = self._standard_check(array, _equations, _memory, _swaps)
+        if _check != None:
+            return _check
+
+        pivot_left = self._choose_pivot(array, pivot_type)
+        p1 = array.pop(pivot_left)
+        pivot_right = self._choose_pivot(array, pivot_type)
+        p2 = array.pop(pivot_right)
+
+        _equations += 1
+        if p1 > p2:
+            p1, p2 = p2, p1
+
+        i = 0
+        lessthan = 0
+        greaterthan = len(array) - 1
+
+        _memory += sys.getsizeof(p1)
+        _memory += sys.getsizeof(p2)
+        _memory += sys.getsizeof(pivot_left)
+        _memory += sys.getsizeof(pivot_right)
+        _memory += sys.getsizeof(i)
+        _memory += sys.getsizeof(lessthan)
+        _memory += sys.getsizeof(greaterthan)
+
+        while i <= greaterthan:
+            _equations += 2
+            if array[i] < p1:
+                _equations -= 1
+                _swaps += 1
+                self._swap(array, i, lessthan)
+                lessthan += 1
+            elif array[i] > p2:
+                _swaps += 1
+                self._swap(array, i, greaterthan)
+                greaterthan -= 1
+                i -= 1
+            i += 1
+
+        array.insert(0, p1)
+        array.append(p2)
+
+        _swaps += 2
+        self._swap(array, 0, lessthan)
+        self._swap(array, -1, greaterthan + 2)
+
+        array[0:lessthan], _equations, _memory, _swaps = self._dual_pivot_quicksort(array[0:lessthan], pivot_type, _equations, _memory, _swaps, _depth + 1)
+        array[lessthan+1:greaterthan + 2], _equations, _memory, _swaps = self._dual_pivot_quicksort(array[lessthan+1:greaterthan + 2], pivot_type, _equations, _memory, _swaps, _depth + 1)
+        array[greaterthan + 3:], _equations, _memory, _swaps = self._dual_pivot_quicksort(array[greaterthan + 3:], pivot_type, _equations, _memory, _swaps, _depth + 1)
+
+        return array, _equations, _memory, _swaps
+
+    def _insertion_sort(self, array, pivot_type, _equations, _memory, _swaps):
+        for i in range(1, len(array)):
+            key = array[i]
+            j = i - 1
+
+            while j >= 0 and key < array[j]:
+                _equations += 1
+                array[j + 1] = array[j]
+                j -= 1
+            array[j + 1] = key
+
+            _equations += 1
 
         return array, _equations, _memory, _swaps
 
@@ -288,11 +331,21 @@ def progressbar(ready, overal, estimated_time):
 if __name__ == "__main__":
     quick_sort = QuickSort()
 
+    # colors = {
+    #     'last': "222222",
+    #     'random': "115511",
+    #     'mid': "551111",
+    #     'mid3': "111155",
+    #     'lomute': "333333",
+    #     'hoar': "444411",
+    #     'dijkstra': "441144",
+    #     'dual': "114444"
+    #     }
     types = ['sorted', 'random', 'almostsorted', 'reverse', 'somenumbers', 'triangular']
     pivots = ['last', 'random', 'mid', 'mid3']
-    sorts = ['lomute', 'hoar', 'dijkstra']
+    sorts = ['lomute', 'hoar', 'dijkstra', 'dual']
     # sorts = ['dijkstra']
-    numbers = [100, 300]  # , 500, 1000
+    numbers = [1*10**2, 3*10**2, 5*10**2, 10**3, 3*10**3, 5*10**3, 10**4]  # , 5*10**2, 10**3, 3*10**3, 5*10**3
     # numbers = [10]
     tries = 50
 
@@ -301,7 +354,8 @@ if __name__ == "__main__":
     estimated_time = float("inf")
 
     measurements = ["Time", "Equations", "Memory", "Swaps"]
-    measurements = ["Swaps"]
+    # measurements = ["Swaps"]
+    # measurements = ["Time"]
 
     srts = dict()
     pvts = dict()
@@ -334,35 +388,42 @@ if __name__ == "__main__":
                             trs[measurement].append(dct_of_result[measurement])
                     for measurement in measurements:
                         nums[measurement].append(sum(trs[measurement]) / len(trs[measurement]))
-                    # estimated_time = (overal - ready) * sum(trs['Time']) / len(trs['Time'])
+                    if 'Time' in measurements:
+                        estimated_time = (overal - ready) * sum(trs['Time']) / len(trs['Time'])
 
                 typs[typ] = nums.copy()
             pvts[pivot] = typs.copy()
         srts[sort] = pvts.copy()
 
+
     for measurement in measurements:
-        for pivot in pivots:
-            for typ in types:
+        for typ in types:
+            fig, axes = plt.subplots(1, len(pivots), sharex=True, sharey=True, figsize=(14, 4))
+            for pivot in pivots:
                 for sort in sorts:
-                    plt.plot(numbers, srts[sort][pivot][typ][measurement], label=f"{sort}")
-                plt.xlabel("Size")
-                plt.ylabel(measurement)
-                plt.legend()
-                plt.title(f"Type: {typ}, Pivot: {pivot}")
+                    axes[pivots.index(pivot)].plot(numbers, srts[sort][pivot][typ][measurement], marker='o', label=f"{sort}_{pivot}".upper())  # , color=f"#{str(int(colors[sort]) + int(colors[pivot])).zfill(6)}"
+                axes[pivots.index(pivot)].set_title(f"Pivot: {pivot.upper()}")
+                axes[pivots.index(pivot)].legend(draggable=True)
+                axes[pivots.index(pivot)].grid(visible=True)
+                
+            fig.suptitle(f"Measurement: {measurement.upper()}, Type: {typ.upper()}")
+            fig.supxlabel("Size")
+            fig.supylabel(measurement)
 
-                plt.show()
+            # plt.show()
 
-    #             # plt.savefig("./Lab7/Results/" + typ + "_" + measurement + "_" + str(len(sorts)) + "_" + "sorts" + "_" + str(len(numbers)) + "_" + "numbers" + "_" + str(tries) + "_" + str(min(numbers)) + "to" + str(max(numbers)) + ".png")
-    #             # plt.clf()
+            plt.savefig("./Lab7/Results/" + typ + "_" + measurement + "_" + str(len(numbers)) + "_" + "numbers" + ".png")
+            plt.clf()
 
                     
 
     # sorted, random, almostsorted, reverse, somenumbers, triangular
     # LAST, RANDOM, MID or MID3 .list
 
-    # array = RandomLists(10, 'triangular', disorder_level=0.4, start=1)
+    # array = RandomLists(25*10**3, 'sorted', disorder_level=0.4, start=1)
+    # array = RandomLists(25, 'somenumbers', disorder_level=0.4, start=1)
     # print(array.list
-    # print(quick_sort.standard_quicksort(array.list, scheme_type='lomute', pivot_type='mid', time_count=True, details_need=True))
+    # print(quick_sort.standard_quicksort(array.list, scheme_type='dual', pivot_type='random', time_count=True, details_need=True))
 
     # array = [3, 5, 8, 9, 4, 9, 7, 2, 6]
     # array = [6, 5, 8, 6, 4, 9, 7, 6, 3]
@@ -370,4 +431,7 @@ if __name__ == "__main__":
     # array = [7, 2, 1, 8, 6, 3, 5, 4]
     # array = [5, 4, 3]
     # array = [9, 5, 1, 0, 6, 3, 1, 1, 1, 4]
+    # print(quick_sort.standard_quicksort(array, scheme_type='lomute', pivot_type='last', time_count=True, details_need=True))
     # print(quick_sort.standard_quicksort(array, scheme_type='hoar', pivot_type='last', time_count=True, details_need=True))
+    # print(quick_sort.standard_quicksort(array, scheme_type='dijkstra', pivot_type='last', time_count=True, details_need=True))
+    # print(quick_sort.standard_quicksort(array, scheme_type='dual', pivot_type='last', time_count=True, details_need=True))
